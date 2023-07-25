99e3351fd0988cec3a2e1af019be2de7e6df9837
Abhishek <pundirabhishek987@gmail.com>
2023-07-25 22:56:20 +0530

    feat: Home component

--- my-book-shelf/src/pages/Home.jsx
+++ my-book-shelf/src/pages/Home.jsx
@@ -1,23 +1,53 @@
-import React, { useContext } from "react";
-import { BookContext } from "../main";
-// import { BookCard } from "../components/BookCard";
-import { Shelf } from "../components/Shelf";
+import React from 'react'
+import { useData } from '../contexts/BookContext'
+import { SingleData } from '../component/SingleData';
 
 export const Home = () => {
-  const { bookData } = useContext(BookContext);
+  const {
+    dataState: { bookData }
+  } = useData();
 
+  const transformedData = () => {
+    let filteredData = [...bookData];
+
+    const CurrentlyReading = filteredData.filter((data) => data.status === "Currently Reading");
+    const Read = filteredData.filter((data) => data.status === "Read");
+    const WantToRead = filteredData.filter((data) => data.status === "Want to Read");
+
+    return { CurrentlyReading, Read, WantToRead };
+  }
+  const { CurrentlyReading, Read, WantToRead } = transformedData();
   return (
-    <>
     <div>
-    {bookData && (
-        <div >
-          <h1>My Bookshelf</h1>
-        <Shelf shelfTitle="Currently Reading"  books={bookData} />
-        <Shelf shelfTitle="Want to Read"  books={bookData.filter(book => book.wantToRead)} />
-        <Shelf shelfTitle="Read"  books={bookData.filter(book => book.read)} />
-        </div>
-      )}
+      <h1>Currently Reading</h1>
+      <hr />
+      <ul>
+        {CurrentlyReading.map((data) => (
+          <li key={data.id}>
+            <SingleData item={data} />
+          </li>
+        ))};
+      </ul>
+      <h1>Want to Read</h1>
+      <hr />
+      <ul>
+        {WantToRead.map((data) => (
+          <li key={data.id}>
+            <SingleData item={data} />
+          </li>
+        ))};
+      </ul>
+      <h1>Read</h1>
+      <hr />
+      <ul>
+        {Read.map((data) => (
+          <li key={data.id}>
+            <SingleData item={data} />
+          </li>
+        ))};
+      </ul>
     </div>
-    </>
-  );
-};
+  )
+}
+
+
